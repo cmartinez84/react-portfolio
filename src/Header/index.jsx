4d4451520b512fb9185bd86ffa3f6518680cc584
chris martinez <chris martinez>
2018-01-31 20:36:36 -0800

    first commit

---
+++ src/Header/index.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Here is my new app</h2>
+      </div>
+    );
+  }
+}
+
+export default Header;
