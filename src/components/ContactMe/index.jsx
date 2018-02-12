ae7815791631f5888a77726015cd770aeb124a3a
chris martinez <chris martinez>
2018-02-12 12:29:22 -0800

    merge captcha

--- src/components/ContactMe/index.jsx
+++ src/components/ContactMe/index.jsx
@@ -1,34 +1,57 @@
 import React, { Component } from 'react';
+import ReCAPTCHA from 'react-google-recaptcha';
 
 class ContactMe extends Component {
   state={
-    email: '',
-    subject: '',
-    message: ''
+    email: 'cardamomclouds84@gmail.com',
+    subject: 'recapcha testin\'',
+    message: 'boo',
+    recaptcha: '',
+    sendingEmail: false
+  }
+  componentWillMount=()=>{
+    console.log(process.env);
   }
 
   handleChange=(e)=>{
     this.setState({[e.target.name]: e.target.value})
   }
 
+  handleCaptchaChange=(res)=>{
+    this.setState({ recaptcha: res });
+  }
+
   handleSubmit=()=>{
+    this.setState({sendingEmail: true});
 
     const messageData = {
       "email": this.state.email,
       "subject":  this.state.subject,
       "message": this.state.message,
+      "recaptcha" : this.state.recaptcha
     }
 
     const EMAIL_API_KEY = process.env.REACT_APP_EMAIL_API_KEY;
     const EMAIL_API_URL= process.env.REACT_APP_EMAIL_API_URL;
+    // const EMAIL_API_URL= "http://192.168.0.28:3003"
+
     const endpointURL = `${EMAIL_API_URL}/send/${EMAIL_API_KEY}`;
 
-    console.log(process.env);
     fetch(endpointURL, {
         method: 'POST',
         body: JSON.stringify(messageData),
-        headers: { "Content-Type": "application/json" }
-      }).then((res)=>{console.log(res);});//end fetch
+        headers: { "Content-Type": "application/json" }})
+        .then((res)=> res.json())
+        .then((data)=>{
+          if(data.success === true){
+            this.setState({
+              email: '',
+              subject: '',
+              message: '',
+              sendingEmail: false
+            });
+          }
+        });//end fetch/then
     }
 
   render() {
@@ -39,22 +62,35 @@ class ContactMe extends Component {
         <div className="form-group">
           <label>Email</label>
           <input type="email" placeholder="me@email.com" className="form-control" id="contact-email" value={this.state.email}
-            onChange={this.handleChange} name="email"/>
+            onChange={this.handleChange} name="email" disabled={this.state.sendingEmail}/>
         </div>
 
         <div className="form-group">
           <label>Subject</label>
-            <input type="text" placeholder="what am i getting myself into?" className="form-control" id="contact-subject" value={this.state.subject} name="subject" onChange={this.handleChange}/>
+            <input type="text" placeholder="what am i getting myself into?" className="form-control" id="contact-subject" value={this.state.subject} name="subject" onChange={this.handleChange} disabled={this.state.sendingEmail}/>
         </div>
 
       <div className="form-group">
         <label>Message</label>
-        <textarea className="form-control" id="contact-body" value={this.state.message} name="message" onChange={this.handleChange}/>
+        <textarea className="form-control" id="contact-body" value={this.state.message} name="message" onChange={this.handleChange} disabled={this.state.sendingEmail}/>
       </div>
-      <button type="button"  onClick={this.handleSubmit}>SUBMIT</button>
+      <button type="button"  onClick={this.handleSubmit} disabled={this.state.sendingEmail}>SUBMIT</button>
+
+        <ReCAPTCHA
+          ref="recaptcha"
+          sitekey={process.env.REACT_APP_CAPTCHA_SITE_KEY}
+          onChange={this.handleCaptchaChange}/>
+
     </div>
     );
   }
 }
+
+
+// <ReCAPTCHA
+//   ref="recaptcha"
+//   sitekey={process.env.REACT_APP_CAPTCHA_SITE_KEY}
+//   onChange={this.handleCaptchaChange}/>
+
 
 export default ContactMe;
